d02b954838890ea608f4788b6c96b624df774caf
HitzCommitted <hitzcommitted@gmail.com>
2023-10-15 13:59:17 +0300

    working baseline

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,7 +14,7 @@ config = dotenv_values(".env")
 SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config['DEBUG']
+DEBUG = config["DEBUG"]
 
 ALLOWED_HOSTS = ['*']
 
@@ -101,10 +101,14 @@ if not DEBUG:
 
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config['DB_NAME'],
+        'USER': config['DB_USER'],
+        'PASSWORD': config['DB_PASSWORD'],
+        'HOST': config['DB_HOST'],
+        'PORT': config['DB_PORT'],
+    }
     }
 
 # Email settings
